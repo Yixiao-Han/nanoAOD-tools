daae3a2f86ad5a1084afc9e1ee531148c86a593b
Vivan Nguyen <vivan.nguyen@cern.ch>
2020-10-07 22:39:19 +0200

    for CRAB, add LeptonEfficiencyCorrectorCppWorker to compiler class for lepSFProducer

--- python/postprocessing/modules/common/lepSFProducer.py
+++ python/postprocessing/modules/common/lepSFProducer.py
@@ -55,12 +55,12 @@ class lepSFProducer(Module):
         self.el_f = ROOT.std.vector(str)(len(el_f))
         self.el_h = ROOT.std.vector(str)(len(el_f))
         for i in range(len(el_f)): self.el_f[i] = el_f[i]; self.el_h[i] = el_h[i];
-        if "/LeptonEfficiencyCorrector_cc.so" not in ROOT.gSystem.GetLibraries():
-            print "Load C++ Worker"
-            ROOT.gROOT.ProcessLine(".L %s/src/PhysicsTools/NanoAODTools/python/postprocessing/helpers/LeptonEfficiencyCorrector.cc+" % os.environ['CMSSW_BASE'])
+#        if "/LeptonEfficiencyCorrector_cc.so" not in ROOT.gSystem.GetLibraries():
+#            print "Load C++ Worker"
+#            ROOT.gROOT.ProcessLine(".L %s/src/PhysicsTools/NanoAODTools/python/postprocessing/helpers/LeptonEfficiencyCorrector.cc+" % os.environ['CMSSW_BASE'])
     def beginJob(self):
-        self._worker_mu = ROOT.LeptonEfficiencyCorrector(self.mu_f,self.mu_h)
-        self._worker_el = ROOT.LeptonEfficiencyCorrector(self.el_f,self.el_h)
+        self._worker_mu = ROOT.LeptonEfficiencyCorrectorCppWorker(self.mu_f,self.mu_h)
+        self._worker_el = ROOT.LeptonEfficiencyCorrectorCppWorker(self.el_f,self.el_h)
 
     def endJob(self):
         pass
